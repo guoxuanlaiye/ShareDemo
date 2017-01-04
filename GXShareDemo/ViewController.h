3170f4217e978825e4f3d26bf57890182cf6c434
yingcan <yingcan@yingcandeimac.local>
2017-01-04 11:27:39 +0800

    Initial Commit

---
+++ GXShareDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GXShareDemo
+//
+//  Created by yingcan on 17/1/4.
+//  Copyright © 2017年 Guoxuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GXShareDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GXShareDemo
+//
+//  Created by yingcan on 17/1/4.
+//  Copyright © 2017年 Guoxuan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
